e0de62e41284f07353ebe5d98ea0f69bb9cc9dd1
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-05-09 19:20:49 +0000

    recarga

--- mi-aplicacion/app/page.tsx
+++ mi-aplicacion/app/page.tsx
@@ -1,14 +1,28 @@
 "use client"
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
   const [fileNames, setFileNames] = useState([]);
+  const fileInput = useRef(null);
 
   useEffect(() => {
     fetch('https://x9gxpoxtaf.execute-api.us-east-1.amazonaws.com/default/listNamePdf')
       .then(response => response.json())
       .then(data => setFileNames(data.file_names));
   }, []);
+
+  const handleUpload = () => {
+    const file = fileInput.current.files[0];
+    const filename = file.name;
+
+    fetch(`https://rh0ss8b8r6.execute-api.us-east-1.amazonaws.com/v1/manuales-pdf/${filename}`, {
+      method: 'PUT',
+      body: file
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  };
 
   return (
     <div>
@@ -23,6 +37,8 @@ export default function Home() {
             ))}
           </ul>
         </div>
+        <input type="file" ref={fileInput} />
+        <button onClick={handleUpload}>Subir archivo pdf</button>
       </main>
     </div>
   );
